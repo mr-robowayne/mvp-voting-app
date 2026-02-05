591e820cf79e8fb800043b8514be51a69d941b7c
Filipe <filipegoncalves57@gmail.com>
2026-02-05 21:43:24 +0000

    Update main.tf

--- terraform/env/dev/main.tf
+++ terraform/env/dev/main.tf
@@ -1,6 +1,5 @@
 module "network_vpc" {
   source = "../../modules/network"
-  #Name main var.tf | Name mode var.tf
   vpc_name    = var.vpc_name
   vpc_cidr    = var.vpc_cidr
   private_sub = var.private_sub
